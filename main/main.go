fc6b7a716803cc79fe89041266407e8cbfc0c4ae
Nicholas Clawson <nickclaw@gmail.com>
2014-05-04 15:12:04 -0700

    Updated monkey scene

--- main/main.go
+++ main/main.go
@@ -14,13 +14,13 @@ func main() {
 
     scene := raytrace.Scene{
         Camera: raytrace.OrthoCamera{
-            Loc: raytrace.Vector{2,0,0},
+            Loc: raytrace.Vector{5,0,0},
             Dir: raytrace.Vector{-1,0,0},
             Width: width,
             Height: height,
         },
     }
-    scene.Build("/Users/nickclaw/Documents/Go/src/github.com/nickclaw/raytrace/resource/icosphere.obj")
+    scene.Build("/Users/nickclaw/Documents/Go/src/github.com/nickclaw/raytrace/resource/monkey.obj")
     scene.Render(image, scale)
 
     output, _ := os.Create("/Users/nickclaw/Documents/Go/src/github.com/nickclaw/raytrace/resource/output.jpg")
