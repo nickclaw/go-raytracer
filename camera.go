7d19583d63714e9f51bae3a1898b6ba933854c3a
Nicholas Clawson <nickclaw@gmail.com>
2014-05-05 22:01:19 -0700

    Remade OrthoCamera

--- camera.go
+++ camera.go
@@ -10,27 +10,23 @@ type Viewer interface {
 type OrthoCamera struct {
     Loc Point
     Dir Vector
-    Width, Height int // actual, not rendered
+    Width, Height float64 // actual, not rendered
 }
 
-func (c OrthoCamera) GetRays(scale int) []Ray {
+func (c OrthoCamera) GetRays(scale float64) []Ray {
     rays := []Ray{}
-    xMult, yMult := 1.0 / float64(scale), 1.0 / float64(scale)
-    xOff, yOff := -float64(c.Width) / 2, -float64(c.Height) / 2
 
-    fmt.Println("mults", xMult, yMult)
-    fmt.Println("offs", xOff, yOff)
-    fmt.Println("uvs", scale * c.Width, scale * c.Height)
+    uOff, vOff := c.Width / 2.0, c.Heigth / 2.0
 
-    for u := 0; u < scale * c.Width; u++ {
-        for v := 0; v < scale * c.Height; v++ {
+    for u := 0; u < int(c.Width * scale); u++ {
+        for v := 0; v < int(c.Height * scale); v++ {
             rays = append(rays, Ray{
-                X : u,
-                Y : v,
+                X: u,
+                Y: v,
                 Loc: Point{
-                    c.Loc[0] + (float64(u) * xMult + xOff) * c.Dir[1],
-                    c.Loc[1] + (float64(u) * yMult + xOff) * c.Dir[0],
-                    c.Loc[2] + (float64(v) * yMult + yOff),
+                    c.Loc[0] + (float64(u) / scale - uOff) * c.Dir[1],
+                    c.Loc[1] + (float64(u) / scale - uOff) * c.Dir[0],
+                    c.Loc[2] + (float64(v) / scale + vOff),
                 },
                 Dir: c.Dir,
             })
